2d9da60b7877f52c3cad5a67d802c363562f9f09
devopsint <devopsint@gmail.com>
2019-07-29 23:03:30 +0300

    remove

--- jenkins_project/createJenkinsImage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/jenkinsci/docker.git
-cd docker
-\cp Dockerfile-centos Dockerfile
-### Insert anywhere before ENTRYPOINT
-echo 'RUN yum -y install ansible' >> Dockerfile
-docker build -t jenk-ans .
